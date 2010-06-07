2dadca919a02ea1cde6f4849fd248e7639707e5f
Marc Kannegiesser <kannegiesser@synyx.de>
2010-06-07 10:55:18 +0000

    * resolving locale using RequestContextUtils (which uses the configured LocaleResolver) instead of request.getLocale() to format dates within tabletag * fixes #308
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@11255 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/core/src/main/java/org/synyx/minos/core/web/MinosColumnDecorator.java
+++ modules/core/src/main/java/org/synyx/minos/core/web/MinosColumnDecorator.java
@@ -2,6 +2,7 @@ package org.synyx.minos.core.web;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
 import org.displaytag.decorator.DisplaytagColumnDecorator;
@@ -11,13 +12,13 @@ import org.joda.time.DateMidnight;
 import org.joda.time.ReadableInstant;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import org.synyx.minos.core.domain.User;
 
 
 /**
- * Basic column decorator that automatically decorates instances of very basic
- * types to String values. Currently the following types are decorated
- * automatically:
+ * Basic column decorator that automatically decorates instances of very basic types to String values. Currently the
+ * following types are decorated automatically:
  * <ul>
  * <li>org.joda.time.DateTime</li>
  * </ul>
@@ -29,17 +30,15 @@ public class MinosColumnDecorator implements DisplaytagColumnDecorator {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * org.displaytag.decorator.DisplaytagColumnDecorator#decorate(java.lang
-     * .Object, javax.servlet.jsp.PageContext,
-     * org.displaytag.properties.MediaTypeEnum)
+     * @see org.displaytag.decorator.DisplaytagColumnDecorator#decorate(java.lang .Object,
+     * javax.servlet.jsp.PageContext, org.displaytag.properties.MediaTypeEnum)
      */
-    public Object decorate(Object columnValue, PageContext pageContext,
-            MediaTypeEnum media) throws DecoratorException {
+    public Object decorate(Object columnValue, PageContext pageContext, MediaTypeEnum media) throws DecoratorException {
 
         if (columnValue instanceof ReadableInstant) {
             ReadableInstant date = (ReadableInstant) columnValue;
-            Locale locale = pageContext.getRequest().getLocale();
+            // Locale locale = pageContext.getRequest().getLocale();
+            Locale locale = RequestContextUtils.getLocale((HttpServletRequest) pageContext.getRequest());
             DateTimeFormatter formatter = getFormatterForInstant(date, locale);
             return formatter.print(date);
         }
@@ -57,13 +56,11 @@ public class MinosColumnDecorator implements DisplaytagColumnDecorator {
     /**
      * Returns a {@link DateTimeFormatter} for a given {@link ReadableInstant}
      * 
-     * @param instant the {@link ReadableInstant} to get the
-     *            {@link DateTimeFormatter} for
+     * @param instant the {@link ReadableInstant} to get the {@link DateTimeFormatter} for
      * @param locale the Locale to get the {@link DateTimeFormatter} for
      * @return {@link DateTimeFormatter}
      */
-    private DateTimeFormatter getFormatterForInstant(ReadableInstant instant,
-            Locale locale) {
+    private DateTimeFormatter getFormatterForInstant(ReadableInstant instant, Locale locale) {
 
         // if its a datemidnight we use shortdateformat for Date, otherwise
         // for DateTime
