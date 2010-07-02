06764527a80625b33b0aad7d08bf0d75ccaf4bb4
Oliver Gierke <gierke@synyx.de>
2010-07-02 13:37:15 +0000

    * apply semantics change of @MenuItemFilter@ to @PermissionMenuItemFilter@ * references #280
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@11869 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/core/src/main/java/org/synyx/minos/core/web/menu/filter/PermissionMenuItemFilter.java
+++ modules/core/src/main/java/org/synyx/minos/core/web/menu/filter/PermissionMenuItemFilter.java
@@ -1,82 +1,37 @@
-/**
- * 
- */
 package org.synyx.minos.core.web.menu.filter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.synyx.minos.core.authentication.AuthenticationService;
 import org.synyx.minos.core.web.menu.MenuItem;
 import org.synyx.minos.core.web.menu.MenuItemFilter;
+import org.synyx.minos.util.Assert;
 
 
 /**
- * {@link MenuItemFilter} that removes all {@link MenuItem}s that the currently logged in user is not allowed to see
- * (recursive!)
+ * {@link MenuItemFilter} that that considers the permissions registered with the {@link MenuItem}.
  * 
  * @author Marc Kannegiesser - kannegiesser@synyx.de
+ * @author Oliver Gierke
  */
 public class PermissionMenuItemFilter implements MenuItemFilter {
 
-    private AuthenticationService authenticationService;
+    private final AuthenticationService authenticationService;
 
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.synyx.minos.core.web.menu.MenuItemFilter#filterMenuItems(java.util.List,
-     * org.synyx.minos.core.domain.User)
-     */
-    @Override
-    public List<MenuItem> filterMenuItems(List<MenuItem> items) {
+    public PermissionMenuItemFilter(AuthenticationService authenticationService) {
 
-        if (items == null) {
-            return null;
-        }
-        List<MenuItem> toRemove = new ArrayList<MenuItem>();
-        for (MenuItem item : items) {
-
-            if (isAllowedToSee(item)) {
-                filterMenuItems(item.getSubMenues());
-            } else {
-                toRemove.add(item);
-            }
-
-        }
-        items.removeAll(toRemove);
-        return items;
-    }
-
-
-    private boolean isAllowedToSee(MenuItem item) {
-
-        if (null == authenticationService) {
-            throw new IllegalStateException("no authenticationservice is set");
-        }
-
-        return authenticationService.hasAllPermissions(item.getPermissions());
-    }
-
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.synyx.hera.core.Plugin#supports(java.lang.Object)
-     */
-    @Override
-    public boolean supports(String delimiter) {
-
-        return true;
-    }
-
-
-    /**
-     * @param authenticationService the authenticationService to set
-     */
-    public void setAuthenticationService(AuthenticationService authenticationService) {
-
+        Assert.notNull(authenticationService);
         this.authenticationService = authenticationService;
     }
 
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.google.common.base.Predicate#apply(java.lang.Object)
+     */
+    @Override
+    public boolean apply(MenuItem item) {
+
+        return authenticationService.hasAllPermissions(item.getPermissions());
+    }
 }
