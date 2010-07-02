0ed5c22dde1604a704c13daf6cba53907953d5da
Oliver Gierke <gierke@synyx.de>
2010-07-02 13:37:04 +0000

    * polished @SimpleUrlResolvingStrategy@, removed unnecessary accessors, JavaDoc * polished @UserPlaceholderAwareUrlResolvingStrategy@ * let it extend @SimpleUrlResolvingStrategy@ and replace placeholder afterwards * made placeholder configurable * references #280
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@11857 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/core/src/main/java/org/synyx/minos/core/web/menu/SimpleUrlResolvingStrategy.java
+++ modules/core/src/main/java/org/synyx/minos/core/web/menu/SimpleUrlResolvingStrategy.java
@@ -4,35 +4,27 @@ package org.synyx.minos.core.web.menu;
  * Strategy that simply returns the url from a property.
  * 
  * @author Marc Kannegiesser - kannegiesser@synyx.de
+ * @author Oliver Gierke
  */
 public class SimpleUrlResolvingStrategy implements UrlResolvingStrategy {
 
-    private String url;
+    private final String url;
 
 
     public SimpleUrlResolvingStrategy(String url) {
 
-        super();
         this.url = url;
     }
 
 
-    public String getUrl() {
-
-        return url;
-    }
-
-
-    public void setUrl(String url) {
-
-        this.url = url;
-    }
-
-
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.synyx.minos.core.web.menu.UrlResolvingStrategy#resolveUrl(org.synyx.minos.core.web.menu.MenuItem)
+     */
     @Override
     public String resolveUrl(MenuItem item) {
 
         return url;
     }
-
 }
--- modules/core/src/main/java/org/synyx/minos/core/web/menu/UserPlaceholderAwareUrlResolvingStrategy.java
+++ modules/core/src/main/java/org/synyx/minos/core/web/menu/UserPlaceholderAwareUrlResolvingStrategy.java
@@ -1,53 +1,64 @@
 package org.synyx.minos.core.web.menu;
 
+import org.apache.commons.lang.StringUtils;
 import org.synyx.minos.core.authentication.AuthenticationService;
 import org.synyx.minos.core.domain.User;
+import org.synyx.minos.util.Assert;
 
 
 /**
- * Strategy that simply returns the url from a property.
+ * {@link UrlResolvingStrategy} that is able to detect a given placeholder in the configured URL and replaces it with
+ * the current {@link User}s username.
  * 
  * @author Marc Kannegiesser - kannegiesser@synyx.de
+ * @author Oliver Gierke
  */
-public class UserPlaceholderAwareUrlResolvingStrategy implements UrlResolvingStrategy {
+public class UserPlaceholderAwareUrlResolvingStrategy extends SimpleUrlResolvingStrategy {
 
-    public static final String USER_PLACEHOLDER = "{username}";
-    private String url;
-    private AuthenticationService authService;
+    public static final String DEFAULT_PLACEHOLDER = "{username}";
+
+    private final AuthenticationService authService;
+    private String placeholder = DEFAULT_PLACEHOLDER;
 
 
+    /**
+     * Creates a new {@link UserPlaceholderAwareUrlResolvingStrategy}.
+     * 
+     * @param url
+     * @param authService
+     */
     public UserPlaceholderAwareUrlResolvingStrategy(String url, AuthenticationService authService) {
 
+        super(url);
+        Assert.notNull(authService);
         this.authService = authService;
-        this.url = url;
     }
 
 
-    public String getUrl() {
+    /**
+     * Configure the placeholder that should be replaced with the username. Defaults to {@value #DEFAULT_PLACEHOLDER}.
+     * 
+     * @param userPlaceholder the userPlaceholder to set
+     */
+    public void setPlaceholder(String userPlaceholder) {
 
-        return url;
+        this.placeholder = StringUtils.isBlank(userPlaceholder) ? DEFAULT_PLACEHOLDER : userPlaceholder;
     }
 
 
-    public void setUrl(String url) {
-
-        this.url = url;
-    }
-
-
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.synyx.minos.core.web.menu.SimpleUrlResolvingStrategy#resolveUrl(org.synyx.minos.core.web.menu.MenuItem)
+     */
     @Override
     public String resolveUrl(MenuItem item) {
 
-        User user = getUser();
+        String url = super.resolveUrl(item);
+
+        User user = authService.getCurrentUser();
         String username = null == user ? "" : user.getUsername();
-        return url.replace(USER_PLACEHOLDER, username).replace("//", "/");
 
+        return url.replace(placeholder, username).replace("//", "/");
     }
-
-
-    private User getUser() {
-
-        return authService.getCurrentUser();
-    }
-
 }
