dd6264419a106f10f534b54450d8b5fb83daf997
Marc Kannegiesser <kannegiesser@synyx.de>
2010-08-24 08:58:43 +0000

    * minor refactorings * minor documentation * added comments in module-context.xml * refs #356
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@12748 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/i18n/src/main/java/org/synyx/minos/i18n/importer/MessageImporter.java
+++ modules/i18n/src/main/java/org/synyx/minos/i18n/importer/MessageImporter.java
@@ -6,10 +6,24 @@ package org.synyx.minos.i18n.importer;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.core.io.Resource;
+import org.synyx.minos.i18n.dao.AvailableLanguageDao;
+import org.synyx.minos.i18n.dao.AvailableMessageDao;
+import org.synyx.minos.i18n.dao.MessageDao;
+import org.synyx.minos.i18n.dao.MessageTranslationDao;
+import org.synyx.minos.i18n.domain.AvailableLanguage;
+import org.synyx.minos.i18n.domain.AvailableMessage;
+import org.synyx.minos.i18n.domain.LocaleWrapper;
+import org.synyx.minos.i18n.domain.Message;
+import org.synyx.minos.i18n.domain.MessageStatus;
+import org.synyx.minos.i18n.domain.MessageTranslation;
+import org.synyx.minos.i18n.util.CollationUtils;
 
 
 /**
@@ -17,15 +31,32 @@ import org.springframework.core.io.Resource;
  */
 public class MessageImporter {
 
-    private MessageMetaAcceptor target;
-
     private Map<String, Resource> resources;
 
+    private static final Log LOG = LogFactory.getLog(MessageImporter.class);
 
-    public MessageImporter(MessageMetaAcceptor target) {
+    private MessageDao messageDao;
+
+    private AvailableMessageDao availableMessageDao;
+
+    private AvailableLanguageDao availableLanguageDao;
+
+    private MessageTranslationDao messageTranslationDao;
+
+
+    public MessageImporter() {
+
+    }
+
+
+    public MessageImporter(MessageDao messageDao, AvailableMessageDao availableMessageDao,
+            AvailableLanguageDao availableLanguageDao, MessageTranslationDao messageTranslationDao) {
 
         super();
-        this.target = target;
+        this.messageDao = messageDao;
+        this.availableMessageDao = availableMessageDao;
+        this.availableLanguageDao = availableLanguageDao;
+        this.messageTranslationDao = messageTranslationDao;
     }
 
 
@@ -33,8 +64,119 @@ public class MessageImporter {
 
         Map<String, String> messages = loadProperties(resource);
 
-        target.setMessages(basename, messages);
+        List<AvailableLanguage> availableLanguages = getAvailableLanguages(basename);
 
+        // update/create messages
+        for (String key : messages.keySet()) {
+
+            String message = messages.get(key);
+            setMessage(basename, key, message, availableLanguages);
+        }
+
+        // find deleted message meta
+        List<AvailableMessage> availableMessages = availableMessageDao.findByBasename(basename);
+        for (AvailableMessage availableMessage : availableMessages) {
+
+            String key = availableMessage.getKey();
+            if (!messages.containsKey(key)) {
+
+                // remove translationinfo
+                messageTranslationDao.deleteBy(availableMessage);
+                // and info about the key
+                availableMessageDao.delete(availableMessage);
+                // and all messages
+                messageDao.deleteBy(basename, key);
+
+                LOG.debug("Deleted meta-information and all translation-requests for basename: " + basename + " key: "
+                        + key);
+            }
+        }
+
+    }
+
+
+    protected void setMessage(String basename, String key, String message, List<AvailableLanguage> availableLanguages) {
+
+        AvailableMessage availableMessage =
+                CollationUtils.getRealMatch(availableMessageDao.findByBasenameAndKey(basename, key), "key", key);
+
+        MessageStatus status = null;
+        if (null == availableMessage) { // new
+            availableMessage = new AvailableMessage(basename, key, message);
+            availableMessage = availableMessageDao.save(availableMessage);
+
+            // create a (base) message so that the keys get resolved
+            messageDao.save(new Message("", "", "", basename, key, message));
+            LOG.debug("Added new message for basename: " + basename + " and key: " + key);
+            status = MessageStatus.NEW;
+        } else if (!availableMessage.getMessage().equals(message)) { // updated
+            status = MessageStatus.UPDATED;
+
+            availableMessage.setMessage(message);
+            availableMessageDao.save(availableMessage);
+            LOG.debug("Updated existing message for basename: " + basename + " and key: " + key);
+
+        } else { // unchanged
+            return;
+        }
+
+        // now update the information what has to be translated
+        for (AvailableLanguage lang : availableLanguages) {
+            MessageTranslation translation =
+                    messageTranslationDao.findByAvailableMessageAndAvailableLanguage(availableMessage, lang);
+
+            if (translation == null) {
+
+                // if the language is not required we need to check if the key is defined
+                if (!lang.isRequired()) {
+
+                    boolean found = false;
+
+                    // check if the key is definied in the current language
+                    List<Message> messages = messageDao.findByBasenameAndLocaleAndKey(basename, lang.getLocale(), key);
+                    for (Message msg : messages) {
+                        if (msg.getKey().equals(key)) {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    // if not skip translation for the language
+                    if (!found) {
+                        continue;
+                    }
+
+                }
+
+                translation = new MessageTranslation(availableMessage, lang, status);
+            }
+
+            translation.setMessageStatus(status);
+
+            messageTranslationDao.save(translation);
+            LOG.debug("Added/Updated new translation (" + lang.getLocale().toString() + ")for basename: " + basename
+                    + " key: " + key);
+        }
+
+    }
+
+
+    private List<AvailableLanguage> getAvailableLanguages(String basename) {
+
+        List<AvailableLanguage> availableLanguages = availableLanguageDao.findByBasename(basename);
+
+        if (availableLanguages.isEmpty()) {
+            // first time we "see" this basename
+            // create a "default" language entry for it
+
+            AvailableLanguage lang = new AvailableLanguage(LocaleWrapper.DEFAULT, basename, true);
+            availableLanguageDao.save(lang);
+            availableLanguages = availableLanguageDao.findByBasename(basename);
+
+            LOG.info("Created new entry for default-language of new basename: " + basename);
+        }
+
+        return availableLanguages;
     }
 
 
--- modules/i18n/src/main/java/org/synyx/minos/i18n/service/MessageTransferService.java
+++ modules/i18n/src/main/java/org/synyx/minos/i18n/service/MessageTransferService.java
@@ -5,8 +5,12 @@ package org.synyx.minos.i18n.service;
 
 import java.io.OutputStream;
 
+import org.synyx.minos.i18n.domain.Message;
+
 
 /**
+ * Service-Interface for transfering {@link Message}s from and to database.
+ * 
  * @author Marc Kannegiesser - kannegiesser@synyx.de
  */
 public interface MessageTransferService {
