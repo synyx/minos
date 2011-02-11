38ac9aa117ffc9bb76f6ceb4a99154f32cb9a00b
Markus Daniel <daniel@synyx.de>
2011-02-11 14:50:54 +0000

    test fixed #4419
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@15086 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/skillz/src/test/java/org/synyx/minos/skillz/service/PdfDocbookCreatorIntegrationTest.java
+++ modules/skillz/src/test/java/org/synyx/minos/skillz/service/PdfDocbookCreatorIntegrationTest.java
@@ -1,25 +1,29 @@
 package org.synyx.minos.skillz.service;
 
-import static org.junit.Assert.*;
+import org.apache.commons.io.IOUtils;
+
+import org.junit.After;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.core.io.ClassPathResource;
+
+import org.synyx.minos.skillz.domain.Level;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.synyx.minos.skillz.domain.Level;
+import java.util.ArrayList;
 
 
 /**
  * Test for {@code PdfDocbookCreatorImpl}.
- * 
- * @author Markus Knittig - knittig@synyx.de
+ *
+ * @author  Markus Knittig - knittig@synyx.de
  */
 public class PdfDocbookCreatorIntegrationTest extends AbstractDocbookCreatorIntegrationTest {
 
@@ -27,16 +31,13 @@ public class PdfDocbookCreatorIntegrationTest extends AbstractDocbookCreatorInte
     private File pdf;
     private OutputStream outputStream;
 
-    @Autowired
-    private FopXsltService fopXsltService;
-
-
-    @Override
-    @Before
+    @Override @Before
     public void setUp() throws Exception {
 
         super.setUp();
-        docbookCreator = new PdfDocbookCreatorImpl(docbookTemplateService, fopXsltService);
+        docbookCreator = new PdfDocbookCreatorImpl(docbookTemplateService,
+                new FopXsltServiceImpl(
+                    new ClassPathResource("/resume-template/maven/src/docbkx-stylesheet/fo/docbook.xsl")));
         pdf = File.createTempFile("test", "pdf");
         outputStream = new FileOutputStream(pdf);
     }
@@ -57,11 +58,10 @@ public class PdfDocbookCreatorIntegrationTest extends AbstractDocbookCreatorInte
 
         Thread.sleep(1500);
         docbookCreator.streamPdf(IOUtils.toString(new ClassPathResource("/projectteam.xml").getInputStream()),
-                new ClassPathResource("/projectteam2fo.xsl").getFile(), outputStream);
+            new ClassPathResource("/projectteam2fo.xsl").getFile(), outputStream);
         outputStream.flush();
 
         assertTrue(pdf.lastModified() > lastModified);
-
     }
 
 
@@ -76,5 +76,4 @@ public class PdfDocbookCreatorIntegrationTest extends AbstractDocbookCreatorInte
 
         assertTrue(pdf.lastModified() > lastModified);
     }
-
 }
