05ceb8bbb7383a1ac783c1e46c79e22c2f3a958f
Marc Kannegiesser <kannegiesser@synyx.de>
2011-06-08 11:53:04 +0000

    * made list of HttpMessageConverter that is used by AnnotationMethodHandlerAdapter extendable (hera plugin mechanism) * removed custom registering of MappingJacksonHttpMessageConverter in @PostConstruct of MonitoringController since this was a quickhack * refs #487
    git-svn-id: http://svn.synyx.org/synyx/opensource/minos/trunk@15992 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- modules/monitoring/src/main/java/org/synyx/minos/monitoring/web/MonitoringController.java
+++ modules/monitoring/src/main/java/org/synyx/minos/monitoring/web/MonitoringController.java
@@ -1,13 +1,6 @@
 package org.synyx.minos.monitoring.web;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 
 import org.springframework.stereotype.Controller;
 
@@ -16,45 +9,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 import org.synyx.minos.monitoring.service.MonitoringService;
 import org.synyx.minos.monitoring.service.MonitoringTest;
 import org.synyx.minos.monitoring.service.MonitoringTestResult;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
 
 
+/**
+ * Controller-Class that handles monitoring-requests
+ *
+ * @author  Marc Kannegiesser - kannegiesser@synyx.de
+ */
 @Controller
-public class MonitoringController implements ApplicationContextAware {
+public class MonitoringController {
 
     @Autowired
     private MonitoringService monitoringService;
-    private ApplicationContext ctx;
-
-    @PostConstruct
-    public void init() {
-
-        Map<String, AnnotationMethodHandlerAdapter> beans = ctx.getBeansOfType(AnnotationMethodHandlerAdapter.class);
-
-        if (beans.size() > 0) {
-            AnnotationMethodHandlerAdapter a = beans.values().iterator().next();
-            HttpMessageConverter<?>[] messageConverters = a.getMessageConverters();
-            List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
-            list.add(new MappingJacksonHttpMessageConverter());
-
-            for (HttpMessageConverter<?> c : messageConverters) {
-                list.add(c);
-            }
-
-            a.setMessageConverters(list.toArray(new HttpMessageConverter[] {}));
-        }
-    }
-
 
     @RequestMapping(value = "/public/monitoring", method = GET)
     @ResponseBody
@@ -69,18 +41,5 @@ public class MonitoringController implements ApplicationContextAware {
     public MonitoringTestResult executeTest(@PathVariable("name") String name) {
 
         return monitoringService.executeTest(name);
-    }
-
-
-    /*
-     * (non-Javadoc)
-     *
-     * @seeorg.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.
-     * ApplicationContext)
-     */
-    @Override
-    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
-
-        this.ctx = ctx;
     }
 }
